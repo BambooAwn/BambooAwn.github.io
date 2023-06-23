e95d3ba2461682fdb2d5eaf641aded6e17de7cdd
BambooAwn <550325221@qq.com>
2023-06-23 15:39:45 +0800

    2023年6月23日15:39:43

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="000" &&  pwd=="0000"){
+    if(userName=="000" &&  pwd=="000"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
